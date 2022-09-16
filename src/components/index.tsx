c8792acb1bb2cdc071e9b121733f8e3c149ba36b
Thenextgen1 <elijahohimairei@gmail.com>
2022-09-16 06:41:43 +0100

    exports file

---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as DashboardNav } from './DashboardNav';
+export { default as Charts } from './DashboardContent/Charts';
+export { default as ContentHeader } from "./DashboardContent/ContentHeader"
