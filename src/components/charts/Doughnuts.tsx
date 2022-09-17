48e33cda7ab6b394a7eb939a8733fba61b260a6b
Thenextgen1 <elijahohimairei@gmail.com>
2022-09-17 18:31:41 +0100

    completed doughnuts component

--- src/components/charts/Doughnuts.tsx
+++ src/components/charts/Doughnuts.tsx
@@ -1,9 +1,20 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import { Istate as Iprops } from './Charts';
+import '../../styles/doughnuts.css'
 
 
 ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+export const options = {
+    plugins: {
+        legend: {
+            display: false,
+        },
+
+    },
+}
 
 
 const Doughnuts: React.FC<Iprops> = ({ sources, location }) => {
@@ -13,7 +24,7 @@ const Doughnuts: React.FC<Iprops> = ({ sources, location }) => {
         datasets: [
             {
                 label: '# of Votes',
-                data: location.map((value) => value.percent),
+                data: location.map((value) => value.count),
                 backgroundColor: [
                     '#599EEA',
                     '#844FF6',
@@ -39,20 +50,18 @@ const Doughnuts: React.FC<Iprops> = ({ sources, location }) => {
         datasets: [
             {
                 label: '# of Votes',
-                data: sources.map((value) => value.percent),
+                data: sources.map((value) => value.count),
                 backgroundColor: [
                     '#599EEA',
                     '#844FF6',
                     '#0FB77A',
                     '#FAB70A',
-                    '#F09468',
                 ],
                 borderColor: [
                     '#599EEA',
                     '#844FF6',
                     '#0FB77A',
                     '#FAB70A',
-                    '#F09468',
 
                 ],
                 borderWidth: 1,
@@ -62,9 +71,38 @@ const Doughnuts: React.FC<Iprops> = ({ sources, location }) => {
 
 
     return (
-        <div><Doughnut data={locationData} />
-            <Doughnut data={sourcesData} />
-        </div>
+        <section className='lg:flex justify-between'>
+            <div className='border-solid border border-[#EFF1F6] rounded-xl my-4 p-6 mr-4 flex-1'>
+                <div className='flex justify-between items-center'>
+                    <h4 className='text-mainstackBlack font-bold text-lg'>Top Locations</h4>
+                    <p className='text-mainstackOrange text-sm'>View full reports</p>
+                </div>
+                <div className='flex flex-col sm:flex-row sm:items-center justify-between mt-4'>
+                    <ul className='location_container'>
+                        {location.map((place, i) => (
+                            <li className='py-2 text-mainstackBlack font-medium' key={i}>{place.country} {place.percent}%</li>
+                        ))}
+                    </ul>
+                    <div className='max-w-[160.56px] w-[40vw] mt-4'><Doughnut data={locationData} options={options} /></div>
+                </div>
+            </div>
+            <div className='border-solid border border-[#EFF1F6] rounded-xl my-4 p-6 flex-1'>
+                <div className='flex justify-between items-center'>
+                    <h4 className='text-mainstackBlack font-bold text-lg'>Top Referral Source</h4>
+                    <p className='text-mainstackOrange text-sm'>View full reports</p>
+                </div>
+                <div className='flex flex-col justify-between sm:flex-row sm:items-center mt-4'>
+                    <ul className='sources_container'>
+                        {sources.map((ref, i) => (
+                            <li className='py-2 text-mainstackBlack font-medium' key={i}>{ref.source} {ref.percent}%</li>
+                        ))}
+                    </ul>
+                    <div className='max-w-[160.56px] w-[40vw] mt-4'>
+                        <Doughnut data={sourcesData} options={options} />
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
