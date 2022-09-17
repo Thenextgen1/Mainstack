072dddb7bda6c6b41c2f0f522368e22f0c6c55dd
Thenextgen1 <elijahohimairei@gmail.com>
2022-09-17 05:21:07 +0100

    added react query wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { DashboardNav } from './components';
 import { Home } from './pages';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <DashboardNav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/item1" element={<Home />} />
-        <Route path="/item2" element={<Home />} />
-        <Route path="/item3" element={<Home />} />
-        <Route path="/item4" element={<Home />} />
-        <Route path="/item5" element={<Home />} />
-        <Route path="/item6" element={<Home />} />
-        <Route path="/item7" element={<Home />} />
-        <Route path="/item8" element={<Home />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <DashboardNav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/item1" element={<Home />} />
+          <Route path="/item2" element={<Home />} />
+          <Route path="/item3" element={<Home />} />
+          <Route path="/item4" element={<Home />} />
+          <Route path="/item5" element={<Home />} />
+          <Route path="/item6" element={<Home />} />
+          <Route path="/item7" element={<Home />} />
+          <Route path="/item8" element={<Home />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
